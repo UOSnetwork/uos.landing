7ae149303903e08b324028c38d762288a20da69c
dimalitvinov <dima8litvinov@gmail.com>
2019-01-30 18:36:54 +0300

    fix 4rd screen

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,16 +1,16 @@
 const openTab = (title, e = event) => {
-  let i, tabcontent, tablinks;
+  let i, content, link;
 
-  tabcontent = document.getElementsByClassName("tab__content");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+  content = document.getElementsByClassName("tab__content");
+  for (i = 0; i < content.length; i++) {
+    content[i].style.display = "none";
   }
 
-  tablinks = document.getElementsByClassName("tab__links");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" tab__links_active", "");
+  link = document.getElementsByClassName("tab__links");
+  for (i = 0; i < link.length; i++) {
+    link[i].className = link[i].className.replace(" tab__links_active", "");
   }
 
-  document.getElementById(title).style.display = "block";
+  document.getElementById(title).style.display = "flex";
   e.currentTarget.className += " tab__links_active";
 }
